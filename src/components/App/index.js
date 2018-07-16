422d3a283b184b9c4bac038d27f2fd4d1eea3eed
Loïc Pirez <loic@pirez.fr>
2018-07-16 09:49:55 +0200

    First exemple

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Wrapper from '../Wrapper'
+import Title from '../Title'
+
+class App extends Component {
+  render () {
+    return (
+      <Wrapper>
+        <Title>
+          Hello World, this is my first styled component!
+        </Title>
+      </Wrapper>
+    )
+  }
+}
+
+export default App
